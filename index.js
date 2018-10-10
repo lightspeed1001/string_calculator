3a86f90c87cadbf78daa29a68682c2b54f917523
Mikael <lightspeed1001@gmail.com>
2018-10-10 13:34:01 +0000

    function now also splits by newline

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ console.log('tutturuu');
 console.log(calculator(""));
 console.log(calculator("1"));
 console.log(calculator("1,2"));
-console.log(calculator("1,2,3,4,5,6,7"));
+console.log(calculator("1,2\n3,4,5,6,7"));
--- string_calc.js
+++ string_calc.js
@@ -2,7 +2,7 @@
 function Add(numbers) 
 {
 	if(!numbers) return 0;
-	numbers = numbers.split(",");
+	numbers = numbers.replace(/\n/g, ",").split(",");
 	if(numbers.length == 1) return parseInt(numbers[0]);
 	sum = 0;
 	numbers.forEach(num => {
