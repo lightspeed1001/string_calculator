4affd0a43920cb7a18f3b8eeb751e6c529d873de
Mikael <lightspeed1001@gmail.com>
2018-10-12 13:29:49 +0000

    Negative numbers are no longer allowed

--- string_calc.js
+++ string_calc.js
@@ -10,9 +10,24 @@ function Add(numbers)
     if(numbers.length == 1) return parseInt(numbers[0]);
     //Add all of the numbers together
     sum = 0;
+    //we'll only allow positive numbers
+    is_valid = true;
+    negatives = [];
     numbers.forEach(num => {
-		sum += parseInt(num);
-	});
+        if(num < 0)
+        {
+            //the number is negative. Function is now invalid
+            is_valid = false;
+            //keep track of invalid numbers
+            negatives.push(num);
+        }
+        else
+		    sum += parseInt(num);
+    });
+
+    if(!is_valid) //function is invalid; give user feedback
+        throw ("Negatives not allowed: " + negatives.join(", "));
     return sum;
+    
 }
 module.exports = Add;
--- string_calc.test.js
+++ string_calc.test.js
@@ -7,3 +7,6 @@ test("1\n1,1 = 3", ()       => { expect(calc("1\n1,1")).toBe(3);});
 test("1,1,1\n1 = 4", ()     => { expect(calc("1,1,1\n1")).toBe(4);});
 test("1\n1,1\n1,1 = 5", ()  => { expect(calc("1\n1,1\n1,1")).toBe(5);});
 test("1,1\n1,1,1,1 = 6", () => { expect(calc("1,1\n1,1,1,1")).toBe(6);});
+
+//Need to wrap the function in a function, so that "toThrow" actually works
+test("-1,-2,3,4,5 = error", () => { expect(() => { calc("-1,-2,3,4,5"); }).toThrow();});
