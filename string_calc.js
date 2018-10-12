d139aaf9be2f41a52255cba4d4b2646090eaa47c
Mikael <lightspeed1001@gmail.com>
2018-10-12 13:10:41 +0000

    Function takes in zero, one or two numbers, split by ,

--- string_calc.js
+++ string_calc.js
@@ -1,6 +1,14 @@
 // string_calc.js
 function Add(numbers) 
 {
-	
+    //empty string returns 0
+    if(!numbers) return 0;
+    
+    //convert the string into an array of numbers, split by ,
+    numbers = numbers.split(",");
+    //check if it's only one number
+    if(numbers.length == 1) return parseInt(numbers[0]);
+    //else add the two numbers together
+    return parseInt(numbers[0]) + parseInt(numbers[1]);
 }
 module.exports = Add;
--- string_calc.test.js
+++ string_calc.test.js
@@ -1,3 +1,5 @@
 // string_calg.test.js
 const calc = require('./string_calc');
 test("{} = 0", () => { expect(calc("")).toBe(0);});
+test("1 = 1", () => { expect(calc("1")).toBe(1);});
+test("1,1 = 2", () => { expect(calc("1,1")).toBe(2);});
